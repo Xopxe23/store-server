8485f7c54a640046828e499c9000995ded11ba44
George Dudaev <georgeduday@gmail.com>
2023-04-02 15:29:22 +0300

    Make base.html, index.html and products.html

--- products/views.py
+++ products/views.py
@@ -2,8 +2,34 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store',
+    }
+    return render(request, 'products/index.html', context=context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - Каталог',
+        'products': [
+            {
+                'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6090,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+            },
+            {
+                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
+                'name': 'Синяя куртка The North Face',
+                'price': 23725,
+                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
+            },
+            {
+                'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
+                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+                'price': 3495,
+                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
+            }
+        ]
+    }
+    return render(request, 'products/products.html', context=context)

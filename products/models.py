89cccc999bc0710eecf563c04bcfc9feba612185
George Dudaev <georgeduday@gmail.com>
2023-04-02 17:34:51 +0300

    Django ORM, create fixtures(dumpdata, loaddata), work with images

--- products/models.py
+++ products/models.py
@@ -6,6 +6,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -15,3 +18,5 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey('ProductCategory', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name}'

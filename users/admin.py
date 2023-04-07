f08ada99bb265586643d26c57ec4d68db8a4c8d2
George Dudaev <georgeduday@gmail.com>
2023-04-07 17:55:10 +0300

    Create CBV IndexView

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import User
 from products.admin import BasketAdmin
 
+
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):

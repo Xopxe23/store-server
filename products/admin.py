160b99ec60a03e9ebd709fe559cd053de7be23d5
George Dudaev <georgeduday@gmail.com>
2023-04-07 16:26:57 +0300

    Working with ModelAdmin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import ProductCategory, Product
+from .models import ProductCategory, Product, Basket
 
 # Register your models here.
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', ('price', 'quantity',), 'image', 'category')
+    readonly_fields = ('category',)
+    search_fields = ('name', 'description')
+    ordering = ('name',)
+    list_display_links = ('name',)
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp', )
+    extra = 0

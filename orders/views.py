525cc256ffa33ddfff25f3e6d2c9eada83a36b34
George Dudaev <georgeduday@gmail.com>
2023-04-15 23:48:09 +0300

    Working with OrderCreateView and template order_create

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,22 @@
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
+from common.views import TitleMixin
 from orders.forms import OrderForm
+from products.models import Basket
 
 
-class OrderCreateView(CreateView):
+class OrderCreateView(TitleMixin, CreateView):
     template_name = 'orders/order_create.html'
     form_class = OrderForm
+    success_url = reverse_lazy('orders:order_create')
+    title = 'Store - Оформление заказа'
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+        return super(OrderCreateView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderCreateView, self).get_context_data()
+        context['baskets'] = Basket.objects.filter(user=self.request.user)
+        return context
